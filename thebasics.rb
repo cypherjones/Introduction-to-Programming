a162b5471c8014740cfee937d082c58c0e5da7f9
cypherjones <aj@heuristixdev.com>
2016-03-10 21:38:11 -0600

    the basics

---
+++ thebasics.rb
@@ -0,0 +1,45 @@
+#name exercise
+name = "AJ " + "Jones"
+puts name
+
+#modulo exercise
+thousands = 8976 / 1000
+hundreds  = 8976 % 1000 / 100
+tens      = 8976 % 1000 % 100 / 10
+ones      = 8976 % 1000 % 100 % 10
+
+puts thousands
+puts hundreds
+puts tens
+puts ones
+
+#movies
+movies = {rocky: '1976', et: '1977', star_wars: '2015', :breakfast_at_tiffanys => '1955'}
+
+puts movies[:rocky]
+puts movies[:et]
+puts movies[:star_wars]
+puts movies[:breakfast_at_tiffanys]
+
+#years
+years = ['1976', '1977', '2015', '1955']
+
+puts years[0]
+puts years[1]
+puts years[2]
+puts years[3]
+
+#factorials
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+#squares
+puts 27.89 * 27.89
+puts 90.87 * 90.87
+puts 22.10 * 22.10
+
+
+#expectations 
+# there should be an closing } instead of a 0
